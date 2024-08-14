4bbd6579ca8d43c970a3cc744171f8eebdf9bc98
Shannon Liu <149122998+shannon-jliu@users.noreply.github.com>
2024-08-14 00:25:16 -0400

    Added categorical crossentropy and softmax; set seed value

--- training/lstm_model_wandb.py
+++ training/lstm_model_wandb.py
@@ -1,20 +1,27 @@
 import wandb
 import numpy as np
 import pandas as pd
+import random
 from sklearn.model_selection import KFold
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, BatchNormalization, Input, Bidirectional
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import ModelCheckpoint
 from keras.regularizers import l1_l2, l1, l2
+from keras.utils import to_categorical
 import tensorflow as tf
 from create_data_splits import create_data_splits
 from get_metrics import get_metrics
-import datetime
 
 def train(df):
+
     wandb.init()
     config = wandb.config
     print(config)
+
+    seed_value = 42
+    np.random.seed(seed_value)
+    random.seed(seed_value)
+    tf.random.set_seed(seed_value)
 
     num_lstm_layers = config.num_lstm_layers
     lstm_units = config.lstm_units
@@ -43,6 +50,14 @@ def train(df):
 
     X_train, X_val, X_test, y_train, y_val, y_test, X_train_sequences, y_train_sequences, X_val_sequences, y_val_sequences, X_test_sequences, y_test_sequences, sequence_length = splits
 
+    if loss == "categorical_crossentropy":
+        y_train_sequences = to_categorical(y_train_sequences)
+        y_val_sequences = to_categorical(y_val_sequences)
+        y_test_sequences = to_categorical(y_test_sequences)
+        num_classes = y_train_sequences.shape[1]
+    else:
+        num_classes = 1
+
     print("X_train_sequences shape:", X_train_sequences.shape)
     print("X_val_sequences shape:", X_val_sequences.shape)
     print("X_test_sequences shape:", X_test_sequences.shape)
@@ -59,32 +74,28 @@ def train(df):
     else:
         reg = None
 
-    if num_lstm_layers == 1:
+    for _ in range(num_lstm_layers - 1):
         if use_bidirectional:
-            model.add(Bidirectional(LSTM(lstm_units, activation=activation, kernel_regularizer=reg)))
+            model.add(Bidirectional(LSTM(lstm_units, return_sequences=True, activation=activation, kernel_regularizer=reg)))
         else:
-            model.add(LSTM(lstm_units, activation=activation, kernel_regularizer=reg))
+            model.add(LSTM(lstm_units, return_sequences=True, activation=activation, kernel_regularizer=reg))
         model.add(Dropout(dropout))
         model.add(BatchNormalization())
-    
-    else:
-        for _ in range(num_lstm_layers - 1):
-            if use_bidirectional:
-                model.add(Bidirectional(LSTM(lstm_units, return_sequences=True, activation=activation, kernel_regularizer=reg)))
-            else:
-                model.add(LSTM(lstm_units, return_sequences=True, activation=activation, kernel_regularizer=reg))
-            model.add(Dropout(dropout))
-            model.add(BatchNormalization())
 
-        if use_bidirectional:
-            model.add(Bidirectional(LSTM(lstm_units, activation=activation)))
-        else:
-            model.add(LSTM(lstm_units, activation=activation))
-        model.add(Dropout(dropout))
-        model.add(BatchNormalization())
+    if use_bidirectional:
+        model.add(Bidirectional(LSTM(lstm_units, activation=activation)))
+    else:
+        model.add(LSTM(lstm_units, activation=activation))
     
+    model.add(Dropout(dropout))
+    model.add(BatchNormalization())
+
     model.add(Dense(dense_units, activation=activation))
-    model.add(Dense(1, activation="sigmoid"))
+    
+    if loss == "categorical_crossentropy":
+        model.add(Dense(num_classes, activation="softmax"))
+    else:
+        model.add(Dense(1, activation="sigmoid"))
 
     if optimizer == 'adam':
         optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
@@ -92,6 +103,8 @@ def train(df):
         optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
     elif optimizer == 'adadelta':
         optimizer = tf.keras.optimizers.Adadelta(learning_rate=learning_rate)
+    elif optimizer == 'rmsprop':
+        optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
 
     model.summary()
     
@@ -134,7 +147,13 @@ def train(df):
         wandb.log(metrics)
 
     y_predict_probs = model.predict(X_test_sequences)
-    y_pred = (y_predict_probs > 0.5).astype(int).flatten()
+
+    if loss == "categorical_crossentropy":
+        y_pred = np.argmax(y_predict_probs, axis=1)
+        y_test_sequences = np.argmax(y_test_sequences, axis=1)
+    else:
+        y_pred = (y_predict_probs > 0.5).astype(int).flatten()
+    
     y_test_sequences = y_test_sequences.astype(int).flatten()
 
     test_metrics = get_metrics(y_pred, y_test_sequences, tolerance=1)
@@ -151,17 +170,17 @@ def main():
         'name': 'lstm_sweep_v3',
         'parameters': {
             'use_bidirectional': {'values': [True, False]},
-            'num_lstm_layers': {'values': [1, 2]},
+            'num_lstm_layers': {'values': [1, 2, 3]},
             'lstm_units': {'values': [64, 128, 256]},
             'dropout_rate': {'values': [0.0, 0.3, 0.5, 0.8]},
             'dense_units': {'values': [32, 64, 128]},
             'activation_function': {'values': ['tanh', 'relu', 'sigmoid']},
-            'optimizer': {'values': ['adam', 'sgd', 'adadelta']},
+            'optimizer': {'values': ['adam', 'sgd', 'adadelta', 'rmsprop']},
             'learning_rate': {'values': [0.001, 0.01, 0.005]},
             'batch_size': {'values': [32, 64, 128]},
             'epochs': {'value': 500},
             'recurrent_regularizer': {'values': ['l1', 'l2', 'l1_l2']},
-            'loss' : {'value' : "binary_crossentropy"},
+            'loss' : {'values' : ["binary_crossentropy", "categorical_crossentropy"]},
             'sequence_length' : {'values' : [1, 5, 15, 30]}
         }
     }
