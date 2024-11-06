0c64b3c07d5a86a25689b3fa659f818cb507ae4a
Shannon Jade Liu <sjl356@sablab-gpu-01.ece.cornell.edu>
2024-11-06 03:32:19 -0500

    Implemented single script to run different types of modalities including all features combined, specific features, and specific features combined; corrected dataset csvs

--- preprocessing/individual_df.py
+++ preprocessing/individual_df.py
@@ -34,38 +34,58 @@ def create_normalized_df(df):
 df = pd.read_csv("../preprocessing/merged_features/all_participants_merged_correct.csv")
 participant_frames_labels = df.iloc[:, :4]
 
-df_pose = df.iloc[:, 4:29]
+df_pose = df.iloc[:, 4:28]
+
+df_facial = df.iloc[:, 28:63]
+
+df_audio = df.iloc[:, 63:]
+
 
 df_pose = pd.concat([participant_frames_labels, df_pose], axis=1)
-df_pose_pca = create_pca_df(df_pose)
 df_pose_norm = create_normalized_df(df_pose)
 df_pose_norm_pca = create_pca_df(df_pose_norm)
 
 df_pose.to_csv("all_participants_pose_features.csv", index=False)
-df_pose_pca.to_csv("all_participants_pose_features_pca.csv", index=False)
 df_pose_norm.to_csv("all_participants_pose_features_norm.csv", index=False)
 df_pose_norm_pca.to_csv("all_participants_pose_features_norm_pca.csv", index=False)
 
-df_facial = df.iloc[:, 29:65]
-
 df_facial = pd.concat([participant_frames_labels, df_facial], axis=1)
-df_facial_pca = create_pca_df(df_facial)
 df_facial_norm = create_normalized_df(df_facial)
 df_facial_norm_pca = create_pca_df(df_facial_norm)
 
 df_facial.to_csv("all_participants_facial_features.csv", index=False)
-df_facial_pca.to_csv("all_participants_facial_features_pca.csv", index=False)
 df_facial_norm.to_csv("all_participants_facial_features_norm.csv", index=False)
 df_facial_norm_pca.to_csv("all_participants_facial_features_norm_pca.csv", index=False)
 
-df_audio = df.iloc[:, 65:]
-
 df_audio = pd.concat([participant_frames_labels, df_audio], axis=1)
-df_audio_pca = create_pca_df(df_audio)
 df_audio_norm = create_normalized_df(df_audio)
 df_audio_norm_pca = create_pca_df(df_audio_norm)
 
 df_audio.to_csv("all_participants_audio_features.csv", index=False)
-df_audio_pca.to_csv("all_participants_audio_features_pca.csv", index=False)
 df_audio_norm.to_csv("all_participants_audio_features_norm.csv", index=False)
 df_audio_norm_pca.to_csv("all_participants_audio_features_norm_pca.csv", index=False)
+
+df_pose_facial = pd.concat([participant_frames_labels, df_pose, df_facial], axis=1)
+df_pose_facial_norm = create_normalized_df(df_pose_facial)
+df_pose_facial_norm_pca = create_pca_df(df_pose_facial_norm)
+
+df_pose_facial.to_csv("all_participants_pose_facial_features.csv", index=False)
+df_pose_facial_norm.to_csv("all_participants_pose_facial_features_norm.csv", index=False)
+df_pose_facial_norm_pca.to_csv("all_participants_pose_facial_features_norm_pca.csv", index=False)
+
+df_pose_audio = pd.concat([participant_frames_labels, df_pose, df_audio], axis=1)
+df_pose_audio_norm = create_normalized_df(df_pose_audio)
+df_pose_audio_norm_pca = create_pca_df(df_pose_audio_norm)
+
+df_pose_audio.to_csv("all_participants_pose_audio_features.csv", index=False)
+df_pose_audio_norm.to_csv("all_participants_pose_audio_features_norm.csv", index=False)
+df_pose_audio_norm_pca.to_csv("all_participants_pose_audio_features_norm_pca.csv", index=False)
+
+df_facial_audio = pd.concat([participant_frames_labels, df_facial, df_audio], axis=1)
+df_facial_audio_norm = create_normalized_df(df_facial_audio)
+df_facial_audio_norm_pca = create_pca_df(df_facial_audio_norm)
+
+df_facial_audio.to_csv("all_participants_facial_audio_features.csv", index=False)
+df_facial_audio_norm.to_csv("all_participants_facial_audio_features_norm.csv", index=False)
+df_facial_audio_norm_pca.to_csv("all_participants_facial_audio_features_norm_pca.csv", index=False)
+
