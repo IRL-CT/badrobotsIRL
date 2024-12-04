8facc2483e2b80cd60ed5072c3bb673469c6520c
Shannon Jade Liu <sjl356@g2-login-02.coecis.cornell.edu>
2024-12-04 10:30:13 -0500

    analysis and plots for annotation data

--- preprocessing/annotations/plot.py
+++ preprocessing/annotations/plot.py
@@ -5,21 +5,15 @@ import matplotlib.cm as cm
 
 annotation_list = [
      #"prompt", "repeated prompt", "more specific/longer prompt", "swapping terms in prompt", "simpler prompt",
-     #"slower", "demanding tone", "interrogative tone", "filler words", "humor",
-     "amusement", "confusion", "frustration",
-     "quitting", "looking at tablet",
+     #"slower", "demanding tone", "interrogative tone", "filler words",
+     "amusement", "confusion", "frustration", "humor"
+    #  "quitting", "looking at tablet",
     # "looking at door",
 ]
 
-# color_palette = cm.get_cmap('tab20', len(annotation_list))
+annotation_name = "Emotion"
 
-# annotation_colors = {annotation_list[i]: color_palette(i) for i in range(len(annotation_list))}
-annotation_colors_bw = ["#D3D3D3", "#A9A9A9", "#808080", "#505050", "#303030",
-                        #"#D3D3D3", "#A9A9A9", "#808080", "#505050", "#303030",
-                        # "#D3D3D3", "#A9A9A9", "#808080", "#505050", "#303030",
-                        ]
-
-data = pd.read_csv('sorted_nodbot_annotations.csv')
+data = pd.read_csv('sorted_nodbot_annotations_revised.csv')
 
 data['labels'] = data['labels'].str.split(", ").apply(lambda x: [label for label in x if label in annotation_list])
 
@@ -35,20 +29,7 @@ num_annotations = len(annotations)
 bar_width = 0.8 / num_annotations
 x_positions = np.arange(len(errors))
 
-plt.figure(figsize=(16, 8))
-# for i, annotation in enumerate(annotations):
-#     plt.bar(
-#         x_positions + i * bar_width, 
-#         grouped_data[annotation], 
-#         width=bar_width, 
-#         label=annotation, 
-#         color=annotation_colors_bw[i]
-#     )
-
-hatch_patterns = [' ', '//', ' ', '\\\\', ' ', '///',
-                  '\\\\\\', ' ', '////', ' ', '\\\\\\\\',
-                #   '/', '\\', '/////', '\\\\\\\\\\', ' '
-                  ]
+plt.figure(figsize=(14, 8))
 
 for i, annotation in enumerate(annotations):
     plt.bar(
@@ -56,21 +37,19 @@ for i, annotation in enumerate(annotations):
         grouped_data[annotation], 
         width=bar_width, 
         label=annotation, 
-        color=annotation_colors_bw[i],
-        #hatch=hatch_patterns[i % len(hatch_patterns)]
     )
 
 
-x_labels = ['Participant alerts','Error 1', 'Error 2', 'Error 3', 'Researcher called']
-plt.xticks(x_positions, x_labels, fontsize=12)
+x_labels = ['Error 1', 'Error 2', 'Error 3']
+plt.xticks(x_positions, x_labels, fontsize=14)
 
-plt.title("Distribution of Participant Nonverbal Response Types Across Successive Robot Errors", fontsize=16)
-plt.xlabel("Participant and Robot Interaction Points", fontsize=14)
-plt.ylabel("Frequency of Nonverbal Response Type", fontsize=14)
-plt.legend(title="Common Participant Nonverbal Response Types", bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=10, handleheight=3, handlelength=3, borderaxespad=0.1)
+plt.ylabel(f"Frequency of {annotation_name}", fontsize=18)
+plt.legend(title=annotation_name, title_fontsize=12, bbox_to_anchor=(0.77, 0.98), loc='upper left', fontsize=12, handleheight=2, handlelength=2, borderaxespad=0.1)
+
 plt.tight_layout()
+plt.rcParams.update({'font.size': 22})
+plt.rcParams.update({'font.family': 'serif'})
 
-plt.savefig('nonverbal_annotations_histogram_bw.png', format='png', dpi=300)
+plt.savefig(f'{annotation_name.lower()}_annotations_histogram_bw_l.png', format='png', dpi=300)
 print("Plot saved.")
 
-plt.show()
