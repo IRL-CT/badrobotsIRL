564125efae08d47247d105611fb06063c3838a6d
shannon-jliu <liushannon56@gmail.com>
2025-10-21 15:55:56 -0400

    confusion matrix logging

--- training/binary_models/gru_binary_model_intraparticipant.py
+++ training/binary_models/gru_binary_model_intraparticipant.py
@@ -5,6 +5,7 @@ import random
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix
 from keras.models import Sequential, Model
 from keras.layers import GRU, Dense, Dropout, BatchNormalization, Input, Bidirectional, concatenate
 from keras.callbacks import ModelCheckpoint
@@ -177,6 +178,9 @@ def train_early_fusion(df, config):
         else:
             y_test_class_indices = y_test_sequences
 
+        cm = confusion_matrix(y_test_class_indices, y_pred)
+        wandb.log({f"fold_{fold_no}_confusion_matrix": cm})
+
         test_metrics = get_test_metrics(y_pred, y_test_class_indices, tolerance=1)
 
         for key in test_metrics_list.keys():
@@ -344,6 +348,9 @@ def train_intermediate_fusion(modality_dfs, config):
         else:
             y_test_class_indices = y_test_sequences
 
+        cm = confusion_matrix(y_test_class_indices, y_pred)
+        wandb.log({f"fold_{fold_no}_confusion_matrix": cm})
+
         test_metrics = get_test_metrics(y_pred, y_test_class_indices, tolerance=1)
         
         for key in test_metrics_list.keys():
@@ -510,6 +517,10 @@ def train_late_fusion(modality_dfs, config):
             y_test_class_indices = np.argmax(y_test_sequences, axis=1)
         else:
             y_test_class_indices = y_test_sequences
+
+        y_test = y_test_class_indices
+        cm = confusion_matrix(y_test, y_pred)
+        wandb.log({f"fold_{fold_no}_confusion_matrix": cm})
 
         test_metrics = get_test_metrics(y_pred, y_test_class_indices, tolerance=1)
 
